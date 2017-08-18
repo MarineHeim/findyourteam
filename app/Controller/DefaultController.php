1fed51c11d540f410e6220e425e21c47cc6508fc
Amaury <amaurybourel@laposte.net>
2017-08-18 16:46:45 +0200

    page profile

--- app/Controller/DefaultController.php
+++ app/Controller/DefaultController.php
@@ -22,6 +22,33 @@ class DefaultController extends Controller
 
 	public function profile()
 	{
+	        $errors = [];
+	        $jeux = null;
+	        $console = null;
+	        $pseudo = null;
+	        if (isset($_POST['updateprofile'])) { // Vérifie que le formulaire registerForm est posté
+	            $jeux = $_POST['jeux'];
+	            $console = $_POST['console'];
+	            $pseudo = $_POST['pseudo'];
+	            $user_manager = new \Model\ProfileModel();
+
+	            if (strlen($pseudo) < 3) {
+	                $errors['pseudo'] = 'Le pseudo est trop court.';
+	            }
+
+				if (strlen($pseudo) > 30) {
+					$errors['pseudo'] = 'Le pseudo est trop long.';
+				}
+
+	            if (empty($errors)) {
+
+	                // On inscrit l'utilisateur
+	                $gameplay = $user_manager->addgameplay([
+	                    'jeux' => $jeux,
+	                    'pseudo' => $pseudo
+	                ]);
+	            }
+	        }
 		$this->show('default/profile');
 	}
 	public function inscription()
--- app/Views/default/profile.php
+++ app/Views/default/profile.php
@@ -2,56 +2,106 @@
 
 <?php $this->start('main_content') ?>
 	<!-- Accueil utilisateur -->
-	<h2>Bonjour <?php echo $w_user['username']?></h2>
+	<h2 class="text-center">Bonjour <?php echo $w_user['username']?></h2>
 	<!-- Fin Accueil utilisateur -->
+
 	<br>
-	<!-- Photo de profil -->
-	<button type="button" name="avatar">Sélectionne une photo de profil</button>
-	<!-- Fin photo de profil -->
-	<br>
-	<br>
-	<br>
-	<!-- Formulaire profil -->
-	<form method="post">
-		<div>
-			<label>Choisis ton jeu:</label>
-			<select>
-				<option value="">League of Legends</option>
-				<option value="">Battlefield 4</option>
-				<option value="">Call of duty</option>
-				<option value="">CS GO</option>
-				<option value="">Diablo 3</option>
-				<option value="">Dota 2</option>
-				<option value="">Fifa 17</option>
-				<option value="">Heroes of the Storm</option>
-				<option value="">Overwatch</option>
-				<option value="">World of warcarft</option>
-			</select>
+
+	<!-- Formulaire pour ajouter un jeu a son profil -->
+
+		<div class="row">
+			<div class="col-md-8">
+				<div class="media">
+					<h3>Tes jeux :</h3>
+				  	<div class="media-left">
+				    	<a href="#">
+				      	<img class="media-object" src="..." alt="...">
+					    </a>
+					</div>
+					<div class="media-body">
+					    <h4 class="media-heading">Media heading</h4>
+					    ...
+					</div>
+				</div>
+
+				<br>
+
+				<div class="">
+					<h3>Ta présentation :</h3>
+					<form method="post">
+					<label></label>
+					<textarea class="form-control" rows="8" cols="100"></textarea><br>
+					<button class="btn btn-default" type="submit" name="button">Ajouter/modifier description</button>
+					</form>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<form id="updateprofile" method="post">
+					<div>
+						<label>Choisis ton jeu :</label>
+						<select class="form-control" name="jeux" id="jeux">
+						  <option>---</option>
+						  <option>League of Legends</option>
+						  <option>Battlefield 4</option>
+						  <option>Call of duty</option>
+						  <option>CS GO</option>
+						  <option>Diablo 3</option>
+						  <option>Dota 2</option>
+						  <option>Fifa 17</option>
+						  <option>Heroes of the Storm</option>
+						  <option>Overwatch</option>
+						  <option>World of warcarft</option>
+						</select>
+					</div>
+
+					<br>
+
+					<div>
+						<label>Plateforme :</label>
+						<select class="form-control" name="console" id="console">
+						  <option>---</option>
+						  <option>PC</option>
+						  <option>PS3</option>
+						  <option>PS4</option>
+						  <option>XBOX 360</option>
+						  <option>XBOX ONE</option>
+						</select>
+					</div>
+
+					<br>
+
+					<div class="form-group">
+					    <label for="pseudo">Pseudo en jeu :</label>
+					    <input type="text" class="form-control" id="pseudo" name="pseudo" placeholder="Pseudo du joueur">
+				  	</div>
+
+					<div>
+						<label>Niveau:</label>
+						<select class="form-control" name="niveau">
+						  <option>---</option>
+						  <option>Débutant</option>
+						  <option>Intermédiaire</option>
+						  <option>Expérimenté</option>
+						  <option>Professionel</option>
+					  	</select>
+				  	</div>
+					<br>
+					<button class="btn btn-default" id="updateprofile" name="updateprofile">Ajouter le jeu a mon profil</button>
+				</form>
+			</div>
 		</div>
-		<br>
+
+	<!-- Fin formulaire -->
+
+
+
+
 		<div>
-			<label>Plateforme:</label>
-			PC <input type="checkbox" name="pc">
-			PS3 <input type="checkbox" name="ps3">
-			PS4 <input type="checkbox" name="ps4">
-			XBOX360 <input type="checkbox" name="xbox360">
-			XBOXONE <input type="checkbox" name="xboxone">
+
 		</div>
-		<br>
-		<div>
-			<label>Niveau:</label>
-			Débutant <input type="checkbox" name="débutant">
-			Moyen <input type="checkbox" name="moyen">
-			Expérimenté <input type="checkbox" name="experimente">
-		</div>
-		<br>
-		<div>
-			<label>Description:</label> <br>
-			<textarea name="description" rows="8" cols="100"></textarea>
-		</div>
-	</form>
+
 	<br>
-	<button type="submit" name="button">Valider mon profil</button>
+
 	<!-- Fin formulaire profil -->
 
 <?php $this->stop('main_content') ?>

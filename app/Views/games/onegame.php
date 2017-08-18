665ae7cfbb155c78ddf39ddc1ae95628bf685e8f
marineheim <contact@osw3.net>
2017-08-18 11:16:37 +0200

    Fix 404

--- app/Views/games/onegame.php
+++ app/Views/games/onegame.php
@@ -8,10 +8,21 @@
   <img width="350" src="<?= $this->assetUrl('img/jeux/' . $game['picture']) ?>" alt="...">
   <p><?php echo $game['description'] ?></p>
 
+
   <p>Nombre de joueurs : <?php echo $game['nb_player'] ?></p>
   <p>Nombre de Teams : <?php echo $game['nb_team'] ?></p>
-  <button class="btn btn-primary" name="seeGamers">Voir les joueurs</button>
-  <button class="btn btn-primary" name="seeTeams">Voir les teams</button>
+
+  <div class="lastplayers">
+      <h4>Derniers joueurs inscrites</h4>
+      <button class="btn btn-primary" name="seeGamers">Voir tous les joueurs</button>
+  </div>
+
+  <div class="lastteams">
+      <h4>Dernières équipes inscrites</h4>
+      <button class="btn btn-primary" name="seeTeams">Voir toutes les teams</button>
+  </div>
+
+
   </center>
 <?php }
 
--- app/sql/findyourteam.sql
+++ app/sql/findyourteam.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.6.5.2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le :  lun. 07 août 2017 à 17:43
--- Version du serveur :  10.1.25-MariaDB
--- Version de PHP :  7.1.7
+-- Client :  127.0.0.1
+-- Généré le :  Ven 18 Août 2017 à 10:35
+-- Version du serveur :  10.1.21-MariaDB
+-- Version de PHP :  5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -38,20 +36,36 @@ CREATE TABLE `games` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `games`
+-- Contenu de la table `games`
 --
 
 INSERT INTO `games` (`id`, `name`, `description`, `picture`, `nb_player`, `nb_team`) VALUES
-(1, 'League Of Legend', 'Description Lol', 'lol.jpg', 0, 0),
-(2, 'Battlefield 4', 'Description Battlefield 4', 'battlefield4.png', 0, 0),
-(3, 'Call Of Duty ', 'Descrption COD ', 'callof.jpg', 0, 0),
-(4, 'CSGO', 'Description CSGO', 'cs.jpg', 0, 0),
-(5, 'Diablo 3', 'Description Diablo 3', 'diablo3.jpg', 0, 0),
-(6, 'Dota 2', 'Description Dota 2', 'dota2.jpg', 0, 0),
-(7, 'Fifa 17', 'Description Fifa 17', 'fifa17.jpg', 0, 0),
-(8, 'Heroes Of The Storm', 'Description heroes of the storm', 'heroesofthestorm.png', 0, 0),
-(9, 'Overwatch', 'Description overwatch', 'overwatch.jpg', 0, 0),
-(10, 'World Of Warcraft', 'Description World of warcraft', 'worldofwarcraft.jpg', 0, 0);
+(1, 'League Of Legend', 'League of Legends est un jeu compétitif en ligne bourré d\'action, qui mélange l\'intensité trépidante des jeux de stratégie en temps réel avec des éléments de jeu de rôle.', 'lol.jpg', 0, 0),
+(2, 'Battlefield 4', 'Plongez dans la guerre multijoueur totale de Battlefield 4. Avec la prise en charge de 64 joueurs et 7 modes de jeu uniques utilisables sur 10 cartes de grande taille, l\'échelle et la portée de Battlefield 4 sont incomparables.', 'battlefield4.png', 0, 0),
+(3, 'Call Of Duty ', 'Call of Duty retourne à ses origines avec Call of Duty: WWII, une expérience à couper le souffle qui servira de référence à la nouvelle génération de joueurs.', 'callof.jpg', 0, 0),
+(4, 'CSGO', 'Counter-Strike : Global Offensive est un FPS multijoueurs en ligne sur PC. Les anti-terroristes et les terroristes s\'affrontent dans différents modes de jeu avec une trentaine d\'armes différentes, sans compter les grenades.', 'cs.jpg', 0, 0),
+(5, 'Diablo 3', 'Diablo 3 est un hack\'n slash (jeu de rôle orienté action) qui se déroule dans un univers dark fantasy 20 ans après les événements narrés dans Diablo 2.', 'diablo3.jpg', 0, 0),
+(6, 'Dota 2', 'DOTA 2, pionner d\'un genre de jeux à part entière dans le domaine de la stratégie, le MOBA. Le principe est de se frayer un chemin jusqu\'à la forteresse ennemie afin de la détruire, le tout en choisissant parmi différents personnages.', 'dota2.jpg', 0, 0),
+(7, 'Fifa 17', 'FIFA 17 est un jeu de simulation de football édité par Electronic Arts.', 'fifa17.jpg', 0, 0),
+(8, 'Heroes Of The Storm', 'Heroes of the Storm est un jeu vidéo de type arène de bataille en ligne multijoueur développé par Blizzard Entertainment. Le jeu se déroule dans un univers fictif dans lequel s’affrontent des héros issus de Warcraft, Diablo, StarCraft et Overwatch.', 'heroesofthestorm.png', 0, 0),
+(9, 'Overwatch', 'Jeu d\'action en vue à la première personne, Overwatch prend place dans un futur proche, en 2074 pour être exact. Dans des parties en 6 contre 6, le joueur incarne un héros parmi la palette proposée.', 'overwatch.jpg', 0, 0),
+(10, 'World Of Warcraft', 'World of Warcraft est un jeu de rôle massivement multijoueurs. Au sein du royaume d\'Azeroth, rejoignez les forces de la Horde ou de l\'Alliance. ', 'worldofwarcraft.jpg', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `message`
+--
+
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL,
+  `expediteur` int(11) NOT NULL,
+  `receveur` int(11) NOT NULL,
+  `titre` varchar(100) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `text` text CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `time` int(11) NOT NULL,
+  `lu` enum('0','1') CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -62,22 +76,27 @@ INSERT INTO `games` (`id`, `name`, `description`, `picture`, `nb_player`, `nb_te
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `avatar` text NOT NULL,
+  `signup_date` int(10) NOT NULL,
   `role` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Déchargement des données de la table `users`
+-- Contenu de la table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `role`) VALUES
-(1, 'kwonos', 'amaurybourel@laposte.net', '0', 'user'),
-(2, 'Kwonos01', 'amaury@laposte.net', '0', 'user'),
-(3, 'kwonos02', 'amaurybourel@laposte.nett', '$2y$10$pZwKPKac4Aw11U6ENrDLwurlG4NTF2LCCKnqDp.55bhE.eJ2yuFni', 'user');
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `avatar`, `signup_date`, `role`) VALUES
+(1, 'amaury60650', '$2y$10$Ep.qNspasejfsJvwuoZoR.E.Kz.Pwgzfdg5oIwsidgZs.0fCxM7/2', 'amaurybourel@laposte.net', '', 0, 'user'),
+(2, 'webforce', '$2y$10$HwN.K6tqUpsxSIoG7b7pve77IOEc2D3W6Jusx.BCL16d5Dwh0U69a', 'ama@ama.fr', '', 0, 'user'),
+(3, 'marineheim', '$2y$10$fBJ45hzKwbsCNAWRd9qdqeF.tdNf6RU/b/VFDhr92tkVaR5sJlx6i', 'marine.heim@yahoo.fr', '', 0, 'user'),
+(4, 'coucoulesloulous', '$2y$10$K0nERVmQq.NPxZ6Rw3jQ2Opi7ayVm2rN3S7JBb7cdak8YTlZAEqHS', 'coucoulesloulous@hotmail.fr', '', 0, 'user'),
+(5, 'poupou', '$2y$10$NJsUkPHAO/02G7ZW5uXmZuOFsaxVzPN1ARLEyLS2HDbxh./psgVJ2', 'poupou@gmail.com', '', 0, 'user'),
+(6, 'poupou8', '$2y$10$WVWG3HXRAM/GiAxvccFCx.x1tLidPaDY3Sy4cyXHSF9e/0haiqyUO', 'poupou8@gmail.com', '', 0, 'user');
 
 --
--- Index pour les tables déchargées
+-- Index pour les tables exportées
 --
 
 --
@@ -87,13 +106,19 @@ ALTER TABLE `games`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `message`
+--
+ALTER TABLE `message`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT pour les tables déchargées
+-- AUTO_INCREMENT pour les tables exportées
 --
 
 --
@@ -102,11 +127,15 @@ ALTER TABLE `users`
 ALTER TABLE `games`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
+-- AUTO_INCREMENT pour la table `message`
+--
+ALTER TABLE `message`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;COMMIT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

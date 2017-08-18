dc1b745b60752face55ae32cf8ae220ac7ceafd4
marineheim <contact@osw3.net>
2017-08-18 11:23:43 +0200

    Fix W

--- app/config.dist.php
+++ app/config.dist.php
@@ -5,7 +5,7 @@ $w_config = [
 	'db_host' => 'localhost',						//hôte (ip, domaine) de la bdd
     'db_user' => 'root',							//nom d'utilisateur pour la bdd
     'db_pass' => '',								//mot de passe de la bdd
-    'db_name' => 'findyourteam',					//nom de la bdd
+    'db_name' => '',								//nom de la bdd
     'db_table_prefix' => '',						//préfixe ajouté aux noms de table
 
 	//authentification, autorisation

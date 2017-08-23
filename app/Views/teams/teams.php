f43d94b8d857d231c1878a5238f478ec0df9efd6
marineheim <contact@osw3.net>
2017-08-23 16:22:38 +0200

    23

--- app/Views/teams/teams.php
+++ app/Views/teams/teams.php
@@ -1,6 +1,12 @@
 <?php $this->layout('layout', ['title' => 'Teams']) ?>
 
 <?php $this->start('main_content') ?>
+	<center>
+	<a href="<?= $this->url('teams_create') ?>"><button name="createTeam" class="btn btn-default create_team">Créer ta team</button></a>
+</center>
+	<br><br>
 	<p>Liste des équipes qui recrutent actuellement.</p>
+
+
 
 <?php $this->stop('main_content') ?>

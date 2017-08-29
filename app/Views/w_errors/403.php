2f354c14e34b272141bf11872dbe9fa112a9c999
Amaury <amaurybourel@laposte.net>
2017-08-29 10:46:04 +0200

    add 404

--- app/Views/w_errors/403.php
+++ app/Views/w_errors/403.php
@@ -1,5 +1,7 @@
 <?php $this->layout('layout', ['title' => 'Nothing to see here']) ?>
 
 <?php $this->start('main_content'); ?>
-<h1>403. Nothing to see here.</h1>
+<div class="page403">
+
+</div>
 <?php $this->stop('main_content'); ?>
--- app/Views/w_errors/404.php
+++ app/Views/w_errors/404.php
@@ -5,7 +5,7 @@
 <?php $this->start('main_content'); ?>
 
 <div class=" page_404">
-<h2>Page non existante :(</h2>
+
 </div>
 
 <?php $this->stop('main_content'); ?>
